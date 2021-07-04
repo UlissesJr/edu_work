3d61856f2ade2a33bedaff49d6dd2888c3e3c3ea
Luobogan <945531767@qq.com>
2021-07-04 18:10:21 +0800

    修复 baseServlet的问题

--- Lagou_edu_home/src/main/java/top/luobogan/base/BaseServlet.java
+++ Lagou_edu_home/src/main/java/top/luobogan/base/BaseServlet.java
@@ -25,41 +25,49 @@ public class BaseServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
 
-        // 1.获取参数 要访问的方法名
-//        String methodName = req.getParameter("methodName");
+        //1.获取参数 要访问的方法名
+        //String methodName = req.getParameter("methodName");
         String methodName = null;
-        // 1. 获取Content-Type 请求的类型
+
+        //1.获取POST请求的 Content-Type类型
         String contentType = req.getHeader("Content-Type");
-        // 判断传递的数据是不是JSON格式
-        if ( contentType.contains("application/json") ) {
-            // 是JSON格式，调用getPostJson方法
-            String postJson = this.getPostJson(req);
-            // 将JSON格式的字符串转换为map
-            Map<String,Object> postMap = JSON.parseObject(postJson, Map.class);
-            // 从map集合中获取methodName
-            methodName = (String)postMap.get("methodName");
-            // 将获取到的数据保存到request域对象中
-            req.setAttribute("map",postMap);
+
+        //2.判断传递的数据是不是JSON格式
+        if("application/json;charset=utf-8".equalsIgnoreCase(contentType)){
+            //是JOSN格式 调用getPostJSON
+            String postJSON = getPostJSON(req);
+
+            //将JSON格式的字符串转化为map
+            Map<String,Object> map = JSON.parseObject(postJSON, Map.class);
+
+            //从map集合中获取 methodName
+            methodName =(String) map.get("methodName");
+
+            //将获取到的数据,保存到request域对象中
+            req.setAttribute("map",map);
+
         }else{
             methodName = req.getParameter("methodName");
         }
 
-        // 2.判断 执行的方法
+        //2.判断 执行对应的方法
         if(methodName != null){
-            // 通过反射优化代码，提升代码的可维护性
+            //通过反射优化代码 提升代码的可维护性
+
             try {
-                // 1.获取字节码文件对象
+                //1.获取字节码文件对象
                 Class c = this.getClass();
-                // 2.根据传入的方法名，获取对应的方法对象
+
+                //2.根据传入的方法名,获取对应的方法对象  findByName
                 Method method = c.getMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
-                // 3.调用method对象的 invoke方法， 执行对应的功能
+
+                //3.调用method对象的 invoke方法,执行对应的功能
                 method.invoke(this,req,resp);
 
             } catch (Exception e) {
                 e.printStackTrace();
-                System.out.println("请求的功能不存在！");
+                System.out.println("请求的功能不存在!!");
             }
-
         }
     }
 
@@ -72,25 +80,29 @@ public class BaseServlet extends HttpServlet {
     POST请求格式为： Content-Type : application/json ; charset=utf-8
     使用该方法进行读取
      */
-    public String getPostJson(HttpServletRequest req){
+    public String getPostJSON(HttpServletRequest request){
 
         try {
-            // 1.从request中 获取缓冲输入流对象
-            BufferedReader reader = req.getReader();
-            // 2.创建StringBuffer 保存读取出的数据
+            //1.从request中 获取缓冲输入流对象
+            BufferedReader reader = request.getReader();
+
+            //2.创建StringBuffer 保存读取出的数据
             StringBuffer sb = new StringBuffer();
-            // 3.循环读取
+
+            //3.循环读取
             String line = null;
-            while ((line = reader.readLine()) != null) {
+            while( (line = reader.readLine() ) != null){
+                //将每次读取的数据 追加到StringBuffer
                 sb.append(line);
             }
-            // 4. 返回结果
+
+            //4.返回结果
             return sb.toString();
 
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
     }
 
 }
