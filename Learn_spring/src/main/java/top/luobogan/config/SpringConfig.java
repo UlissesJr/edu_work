242f89edb543ac362f16dfc3478a9f3fa92ea301
Luobogan <945531767@qq.com>
2021-07-24 16:08:11 +0800

    配置文件分开

--- Learn_spring/src/main/java/top/luobogan/config/SpringConfig.java
+++ Learn_spring/src/main/java/top/luobogan/config/SpringConfig.java
@@ -1,13 +1,11 @@
 package top.luobogan.config;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.commons.dbutils.QueryRunner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Import;
 
 import javax.sql.DataSource;
 
@@ -17,30 +15,8 @@ import javax.sql.DataSource;
  */
 @Configuration
 @ComponentScan("top.luobogan")
-@PropertySource("classpath:jdbc.properties")
+@Import(DatasourceConfig.class)
 public class SpringConfig {
-
-    @Value("${jdbc.driverClassName}")
-    private String driver;
-    @Value("${jdbc.url}")
-    private String url;
-    @Value("${jdbc.username}")
-    private String username;
-    @Value("${jdbc.password}")
-    private String password;
-
-
-    @Bean // 将方法的返回值存储到容器中
-    public DataSource getDataSource(){
-        DruidDataSource druidDataSource = new DruidDataSource();
-        druidDataSource.setDriverClassName(driver);
-        druidDataSource.setUrl(url);
-        druidDataSource.setUsername(username);
-        druidDataSource.setPassword(password);
-
-        return druidDataSource;
-
-    }
 
     @Bean
     public QueryRunner getQueryRunner(@Autowired DataSource dataSource){
