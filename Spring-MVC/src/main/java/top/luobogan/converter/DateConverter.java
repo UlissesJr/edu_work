d30ed2108093c3caa832eaabb0da88222671e078
Luobogan <945531767@qq.com>
2021-08-01 23:42:48 +0800

    SpringMVC 快速入门

---
+++ Spring-MVC/src/main/java/top/luobogan/converter/DateConverter.java
@@ -0,0 +1,26 @@
+package top.luobogan.converter;
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by LuoboGan
+ * Date:2021/8/1
+ */
+public class DateConverter implements Converter<String, Date> {
+
+    public Date convert(String dateStr) {
+        //将日期字符串转换成日期对象 返回
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = null;
+        try {
+            date = format.parse(dateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+}
