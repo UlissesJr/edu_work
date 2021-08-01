e9903268547fdf4df92860311a5a6a30def353c5
Luobogan <945531767@qq.com>
2021-08-01 10:34:25 +0800

    spring整合web: 配置监听器

--- spring_web/src/main/java/top/luobogan/servlet/AccountServlet.java
+++ spring_web/src/main/java/top/luobogan/servlet/AccountServlet.java
@@ -1,6 +1,8 @@
 package top.luobogan.servlet;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import top.luobogan.domain.Account;
 
 import java.io.IOException;
@@ -16,10 +18,15 @@ public class AccountServlet extends javax.servlet.http.HttpServlet {
     @Override
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
-        ClassPathXmlApplicationContext classPathXmlApplicationContext =
-                new ClassPathXmlApplicationContext("applicationContext.xml");
+//        ClassPathXmlApplicationContext classPathXmlApplicationContext =
+//                new ClassPathXmlApplicationContext("applicationContext.xml");
+//
+//        Account account = (Account) classPathXmlApplicationContext.getBean("account");
+//        System.out.println(account);
 
-        Account account = (Account) classPathXmlApplicationContext.getBean("account");
+        ApplicationContext applicationContext =
+                WebApplicationContextUtils.getWebApplicationContext(request.getServletContext());
+        Account account = (Account) applicationContext.getBean("account");
         System.out.println(account);
 
     }
